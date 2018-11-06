0b307ea8d5f3a34bdc3d1c35e8949ad552efe372
Thomas Braccia <bracciata@gmail.com>
2018-11-06 16:36:36 -0600

    Working visible countdown
    There is now a working visible countdown, however it does not create entire screen overlay, also nothing happens when done, and cancel does nothing.

--- party_matt_one/lib/screens/addCharacterScreenFromScratch.dart
+++ party_matt_one/lib/screens/addCharacterScreenFromScratch.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-class CharacterScreen extends State<CharacterScreenStatefulWidget> {
+class Countdown extends AnimatedWidget {
+  Countdown({Key key, this.animation}) : super(key: key, listenable: animation);
+  Animation<int> animation;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Text(animation.value.toString(),
+        style: new TextStyle(fontSize: 150.0));
+  }
+}
+
+class CharacterScreen extends State<CharacterScreenStatefulWidget>
+    with TickerProviderStateMixin {
   final List<TextEditingController> playerNames = new List();
   final List<int> textLength = new List();
   String lastDeletedValue = "";
   int dismissIndex = 0;
+  bool visible = false;
+  AnimationController aniController;
+  static const int startValue = 4;
   @override
   void initState() {
     super.initState();
@@ -13,17 +28,26 @@ class CharacterScreen extends State<CharacterScreenStatefulWidget> {
     playerNames.add(new TextEditingController());
     textLength.add(0);
     textLength.add(0);
+    aniController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: startValue));
   }
 
   void _addNewPlayer() {
-    if(playerNames.length<9){
-    setState(() {
-      playerNames.add(new TextEditingController());
-      textLength.add(0);
-    });
-    }else{
+    if (playerNames.length < 9) {
+      setState(() {
+        playerNames.add(new TextEditingController());
+        textLength.add(0);
+      });
+    } else {
       //todo implement warn user that max players is nine
     }
+  }
+
+  void startCountdown() {
+    setState(() {
+      visible = true;
+      aniController.forward(from: 0.0);
+    });
   }
 
   @override
@@ -33,102 +57,137 @@ class CharacterScreen extends State<CharacterScreenStatefulWidget> {
           title: new Text('Setup'),
           leading: new Container(),
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.arrow_forward),
-            onPressed: (){
-              //begin game
-            },)
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                //begin game
+                startCountdown();
+              },
+            )
           ],
         ),
         body: new Center(
-            child: new Column(children: <Widget>[
-          new Expanded(
-            child: ListView.builder(
-                itemCount: playerNames.length,
-                itemBuilder: (context, index) {
-                  final item = index.toString();
-                  if (index > 1) {
-                    return Dismissible(
-                        // Each Dismissible must contain a Key. Keys allow Flutter to
-                        // uniquely identify Widgets.
-                        key: Key(item),
-                        // We also need to provide a function that tells our app
-                        // what to do after an item has been swiped away.
-                        onDismissed: (direction) {
-                          // Remove the item from our data source.
-                          dismissIndex = index + 1;
-                          lastDeletedValue = playerNames[index].text;
-                          playerNames[index].text = "";
+            child: new Stack(children: [
+          new Column(children: <Widget>[
+            new Expanded(
+              child: ListView.builder(
+                  itemCount: playerNames.length,
+                  itemBuilder: (context, index) {
+                    final item = index.toString();
+                    if (index > 1) {
+                      return Dismissible(
+                          // Each Dismissible must contain a Key. Keys allow Flutter to
+                          // uniquely identify Widgets.
+                          key: Key(item),
+                          // We also need to provide a function that tells our app
+                          // what to do after an item has been swiped away.
+                          onDismissed: (direction) {
+                            // Remove the item from our data source.
+                            dismissIndex = index + 1;
+                            lastDeletedValue = playerNames[index].text;
+                            playerNames[index].text = "";
 
-                          setState(() {
-                            playerNames.removeAt(index);
-                            textLength.removeAt(index);
-                          });
+                            setState(() {
+                              playerNames.removeAt(index);
+                              textLength.removeAt(index);
+                            });
 
-                          // Then show a snackbar!
-                          Scaffold.of(context).showSnackBar(SnackBar(
-                              content: new Text(lastDeletedValue != ""
-                                  ? lastDeletedValue + " dismissed"
-                                  : "Player " +
-                                      (dismissIndex).toString() +
-                                      " dismissed"),
-                              action: new SnackBarAction(
-                                label: "Undo",
-                                onPressed: () {
-                                  setState(() {
-                                    playerNames.insert(
-                                        dismissIndex - 1,
-                                        new TextEditingController(
-                                            text: lastDeletedValue));
-                                  });
-                                  textLength.insert(dismissIndex-1, lastDeletedValue.length);
-                                },
-                              )));
-                        },
-                        // Show a red background as the item is swiped away
-                        background: Container(color: Colors.red),
-                        child: new Container(decoration: new BoxDecoration(color: Colors.white), child:ListTile(
-                          
-                            title: TextField(
-                                controller: playerNames[index],
-                                maxLength: 19,
-                                onChanged: (text) {
-                                  setState(() {
-                                    textLength[index] = text.length;
-                                  });
-                                },
-                                
-                                decoration: new InputDecoration(
+                            // Then show a snackbar!
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: new Text(lastDeletedValue != ""
+                                    ? lastDeletedValue + " dismissed"
+                                    : "Player " +
+                                        (dismissIndex).toString() +
+                                        " dismissed"),
+                                action: new SnackBarAction(
+                                  label: "Undo",
+                                  onPressed: () {
+                                    setState(() {
+                                      playerNames.insert(
+                                          dismissIndex - 1,
+                                          new TextEditingController(
+                                              text: lastDeletedValue));
+                                    });
+                                    textLength.insert(dismissIndex - 1,
+                                        lastDeletedValue.length);
+                                  },
+                                )));
+                          },
+                          // Show a red background as the item is swiped away
+                          background: Container(color: Colors.red),
+                          child: new Container(
+                              decoration:
+                                  new BoxDecoration(color: Colors.white),
+                              child: ListTile(
+                                  title: TextField(
+                                      controller: playerNames[index],
+                                      maxLength: 19,
+                                      onChanged: (text) {
+                                        setState(() {
+                                          textLength[index] = text.length;
+                                        });
+                                      },
+                                      decoration: new InputDecoration(
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          counterText: (18 - textLength[index])
+                                              .toString(),
+                                          //TODO decide between allowing the user to go as long as they want and just tell them they are wrong or using max length.
+                                          counterStyle: (18 -
+                                                      textLength[index] <
+                                                  0)
+                                              ? new TextStyle(color: Colors.red)
+                                              : null)))));
+                    } else {
+                      return Container(
+                          decoration: new BoxDecoration(
+                              color: Color.fromARGB(255, 221, 221, 221)),
+                          child: ListTile(
+                              title: TextField(
+                                  controller: playerNames[index],
+                                  maxLength: 18,
+                                  onChanged: (text) {
+                                    setState(() {
+                                      textLength[index] = text.length;
+                                    });
+                                  },
+                                  decoration: new InputDecoration(
                                     filled: true,
-                                    fillColor: Colors.white,
+                                    fillColor:
+                                        Color.fromARGB(255, 221, 221, 221),
                                     counterText:
                                         (18 - textLength[index]).toString(),
-                                        //TODO decide between allowing the user to go as long as they want and just tell them they are wrong or using max length.
-                                    counterStyle: (18 - textLength[index] < 0)
-                                        ? new TextStyle(color: Colors.red)
-                                        : null)))));
-                    } else {
-                    return Container(decoration: new BoxDecoration(color: Color.fromARGB(255, 221,221, 221)), child: ListTile(
-                        title: TextField(
-                            controller: playerNames[index],
-                            maxLength: 18,
-                            onChanged: (text) {
-                              setState(() {
-                                textLength[index] = text.length;
-                              });
-                            },
-                            decoration: new InputDecoration(
-                              filled: true,
-                              fillColor:Color.fromARGB(255, 221,221, 221),
-                              counterText: (18 - textLength[index]).toString(),
-                            ))));
-                  }
-                }),
-          ),
+                                  ))));
+                    }
+                  }),
+            ),
+          ]),
+          new Center(
+              child: new Opacity(
+            opacity: visible ? 0.75 : 0.0,
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  new Countdown(
+                    animation: new StepTween(begin: startValue, end: 0)
+                        .animate(aniController),
+                  ),
+                  new FlatButton(
+                    child: new Text("Cancel"),
+                    onPressed: () {
+                      //TODO implement cancel
+                    },
+                  )
+                ]),
+          )),
         ])),
         floatingActionButton: new FloatingActionButton(
             onPressed: _addNewPlayer,
             tooltip: 'Add Player',
-            backgroundColor: playerNames.length<9?null:Color.fromARGB(255, 221,221, 221),
+            backgroundColor: playerNames.length < 9
+                ? null
+                : Color.fromARGB(255, 221, 221, 221),
             child: new Icon(Icons.add)));
   }
 }
